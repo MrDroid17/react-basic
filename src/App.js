960b0df098a8625ecbc1aede06338e832c6efb5c
Sobhit Kumar <sobhit2010@gmail.com>
2020-08-01 19:36:11 +0530

    Using State

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello  World!</h1>
-      <p>This is really working</p>
-      <Person name="Sobhit" age="23">I am Software Engg by profession.</Person>
-      <Person name="Mohit" age="30" >I am Teacher by profession.</Person>
-      <Person name="Arpit" age="29" >I am Interior Designer by profession.</Person>
+class App extends Component {
+  state = {
+    persons: [
+      { name: "Sobhit", age: 24, profession: "I am Software Engg by profession." },
+      { name: "Mohit", age: 30, profession: "I am Teacher by profession." },
+      { name: "Arpit", age: 29, profession: "I am Interior Designer by profession." },
+    ]
+  }
 
-    </div>
-  );
-  // return React.createElement('div', null, React.createElement('h1', null, 'Tell me about react.'))
+  render() {
+    return (
+      <div className="App">
+        <p>This is really working</p>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>{this.state.persons[0].profession}</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>{this.state.persons[1].profession}</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>{this.state.persons[2].profession}</Person>
+      </div>
+    );
+    // return React.createElement('div', null, React.createElement('h1', null, 'Tell me about react.'))
+  }
 }
 
 export default App;
