e24bd7c0712e8948653309ee2b4a1387f452ee29
Sobhit Kumar <sobhit2010@gmail.com>
2020-08-02 01:13:21 +0530

    two way of passing method reference

--- src/App.js
+++ src/App.js
@@ -1,80 +1,89 @@
 import React, { Component, useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
+
+
 /**
- * function based component with useState hook
+ * Class based component with setState
  */
-const App = (props) => {
-  const [personsState, setNewPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: "Sobhit", age: 24, profession: "I am Software Engg by profession." },
       { name: "Mohit", age: 30, profession: "I am Teacher by profession." },
       { name: "Arpit", age: 29, profession: "I am Interior Designer by profession." },
     ],
     otherState: "additional Info..."
-  })
-
-  console.log('Person State=========>', personsState)
-
-  const updatePersonHandler = () => {
-    setNewPersonsState({
-      persons: [
-        { name: "Sobhit", age: 28, profession: "I am Senior Software Engg by profession." },
-        { name: "Mohit", age: 32, profession: "I am assistant Professor by profession." },
-        { name: "Arpit", age: 29, profession: "I am senior Interior Designer by profession." },
-      ],
-      otherState: personsState.otherState
-    })
   }
 
-  return (
-    <div className="App">
-      <p>This is really working</p>
-      <button onClick={updatePersonHandler}>Update person</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>{personsState.persons[0].profession}</Person>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>{personsState.persons[1].profession}</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>{personsState.persons[2].profession}</Person>
-    </div>
-  );
-  // return React.createElement('div', null, React.createElement('h1', null, 'Tell me about react.'))
+  updatePersonHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 28, profession: "I am Senior Software Engg by profession." },
+        { name: "Mohit", age: 32, profession: "I am assistant Professor by profession." },
+        { name: "Arpit", age: 29, profession: "I am senior Interior Designer by profession." },
+      ]
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <p>This is really working</p>
+        <button onClick={() => this.updatePersonHandler('Babloo!!')}>Update person</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}>{this.state.persons[0].profession}</Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}>{this.state.persons[1].profession}</Person>
+        <Person
+          name={this.state.persons[2].name}
+          click={this.updatePersonHandler.bind(this, 'Sobhit AKA Babloo')}
+          age={this.state.persons[2].age}>{this.state.persons[2].profession}</Person>
+      </div>
+    );
+    // return React.createElement('div', null, React.createElement('h1', null, 'Tell me about react.'))
+  }
 }
 
 export default App;
 
 /**
- * Class based component with setState
+ * function based component with useState hook
  */
-// class App extends Component {
-  // state = {
-  //   persons: [
-  //     { name: "Sobhit", age: 24, profession: "I am Software Engg by profession." },
-  //     { name: "Mohit", age: 30, profession: "I am Teacher by profession." },
-  //     { name: "Arpit", age: 29, profession: "I am Interior Designer by profession." },
-  //   ],
-  //   otherState: "additional Info..."
-  // }
+// const App = (props) => {
+//   const [personsState, setNewPersonsState] = useState({
+//     persons: [
+//       { name: "Sobhit", age: 24, profession: "I am Software Engg by profession." },
+//       { name: "Mohit", age: 30, profession: "I am Teacher by profession." },
+//       { name: "Arpit", age: 29, profession: "I am Interior Designer by profession." },
+//     ],
+//     otherState: "additional Info..."
+//   })
 
-  // updatePersonHandler = () => {
-  //   this.setState({
-  //     persons: [
-  //       { name: "Sobhit", age: 28, profession: "I am Senior Software Engg by profession." },
-  //       { name: "Mohit", age: 32, profession: "I am assistant Professor by profession." },
-  //       { name: "Arpit", age: 29, profession: "I am senior Interior Designer by profession." },
-  //     ]
-  //   })
-  // }
-//   render() {
-//     return (
-//       <div className="App">
-//         <p>This is really working</p>
-//         <button onClick={this.updatePersonHandler}>Update person</button>
-//         <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>{personsState.persons[0].profession}</Person>
-//         <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>{personsState.persons[1].profession}</Person>
-//         <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>{personsState.persons[2].profession}</Person>
-//       </div>
-//     );
-//     // return React.createElement('div', null, React.createElement('h1', null, 'Tell me about react.'))
+//   console.log('Person State=========>', personsState)
+
+//   const updatePersonHandler = () => {
+//     setNewPersonsState({
+//       persons: [
+//         { name: "Sobhit", age: 28, profession: "I am Senior Software Engg by profession." },
+//         { name: "Mohit", age: 32, profession: "I am assistant Professor by profession." },
+//         { name: "Arpit", age: 29, profession: "I am senior Interior Designer by profession." },
+//       ],
+//       otherState: personsState.otherState
+//     })
 //   }
+
+//   return (
+//     <div className="App">
+//       <p>This is really working</p>
+//       <button onClick={updatePersonHandler}>Update person</button>
+//       <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>{personsState.persons[0].profession}</Person>
+//       <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>{personsState.persons[1].profession}</Person>
+//       <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>{personsState.persons[2].profession}</Person>
+//     </div>
+//   );
+//   // return React.createElement('div', null, React.createElement('h1', null, 'Tell me about react.'))
 // }
 
 // export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
   return (
     <div>
-      <h4>My name is {props.name} Kumar. I am {props.age} years old.</h4>
+      <h4 onClick={props.click}>My name is <span className="pointer">{props.name}</span> Kumar. I am {props.age} years old.</h4>
       <p>{props.children}</p>
     </div>
   )
