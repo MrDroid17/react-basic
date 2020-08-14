79c8fdd161290c74c784d81d0281f27e43016468
Sobhit Kumar <sobhit2010@gmail.com>
2020-08-15 01:52:24 +0530

    Adding media queries

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,32 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
-const StyledDiv = styled.div`
-.person {
-  width: 60%;
-  margin: 16px auto;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 16px;
-  text-align: center; 
-}
-'@media (max-width:900px)': {
-  width: '650px',
-  backgroundColor: 'salmon'
-},
-'@media (max-width:600px)': {
-  width: '450px',
-  backgroundColor: 'lightgray'
-}  
-`;
+import styleClasses from './Person.module.css';
 
 const person = (props) => {
 
   return (
-    <StyledDiv>
+    <div className={styleClasses.person}>
       <h4 onClick={props.click}>My name is <span className="pointer">{props.name}</span> Kumar. I am {props.age} years old.</h4>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name}></input>
-    </StyledDiv>
+    </div>
 
   )
 }
