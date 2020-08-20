a6ffccbbc75545b9b9f0236ee4f3748d23b73228
Sobhit Kumar <sobhit2010@gmail.com>
2020-08-21 00:51:13 +0530

    Using Auxiliary component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment} from 'react';
 import styleClasses from './Person.module.css';
+import Aux from '../../../hoc/Auxiliary';
 
 class Person extends Component {
   render(){
-    return [
+    return (
       // <div className={styleClasses.person}>
-        <h4 key="i1" onClick={this.props.click}>My name is <span className={styleClasses.tooltip}>{this.props.name}<span className={styleClasses.tooltiptext}> Click the name to delete the person.</span></span> . I am {this.props.age} years old.</h4>,
-        <p key="i2">{this.props.children}</p>,
+      <Aux>
+        <h4 key="i1" onClick={this.props.click}>My name is <span className={styleClasses.tooltip}>{this.props.name}<span className={styleClasses.tooltiptext}> Click the name to delete the person.</span></span> . I am {this.props.age} years old.</h4>
+        <p key="i2">{this.props.children}</p>
         <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} />
+      </Aux>
+
       // </div>
-    ];
+    );
   }
 }
 
