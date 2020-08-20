88e0c43e0493005725c0b427ea26b29a2afee5ec
Sobhit Kumar <sobhit2010@gmail.com>
2020-08-20 23:50:44 +0530

    return html as array withoud parent div

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,13 +3,13 @@ import styleClasses from './Person.module.css';
 
 class Person extends Component {
   render(){
-    return (
-      <div className={styleClasses.person}>
-        <h4 onClick={this.props.click}>My name is <span className={styleClasses.tooltip}>{this.props.name}<span className={styleClasses.tooltiptext}> Click the name to delete the person.</span></span> . I am {this.props.age} years old.</h4>
-        <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name}></input>
-      </div>
-    )
+    return [
+      // <div className={styleClasses.person}>
+        <h4 key="i1" onClick={this.props.click}>My name is <span className={styleClasses.tooltip}>{this.props.name}<span className={styleClasses.tooltiptext}> Click the name to delete the person.</span></span> . I am {this.props.age} years old.</h4>,
+        <p key="i2">{this.props.children}</p>,
+        <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} />
+      // </div>
+    ];
   }
 }
 
